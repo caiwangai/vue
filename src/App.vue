b5922ad9ca2ced5f89c42b77de4d016064126895
your name <your email>
2017-07-22 02:11:20 +0800

    dfjhjkfdjkf

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
-     <div>sddsdsdsdsds</div>蔡万概司法解释大家看电视剧的伤口上的借款收到的山东
+     <div>sddsdsdsddsdsdsdsdsdssds</div>蔡万概司法解释大家看电视剧的伤口上的借款收到的山东
   </div>
 </template>
 
