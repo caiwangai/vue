52027687fdfe2c99bb7606a258c4b2ca36062105
your name <your email>
2017-07-22 02:17:21 +0800

    真的假的

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
-     <div>sddsdsdsddsdsdsdsdsdssdsgb gbgbgbggbgb</div>
+     <div>sddsdsdsddsdsddsdsdssdsdsdssdsgb gbgbgbggbgb</div>
      <span></span>
   </div>
 </template>
