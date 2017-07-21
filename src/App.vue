119e7f4f3c1729c25721ea25c4f30f13c56fa2d0
your name <your email>
2017-07-22 02:23:02 +0800

    还原回来

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
-     <div>sddsdsdsddsdsddsdsdssdsdsdss第三节开始的空间的数据库dsgb gbgbgbggbgb</div>
-     <span>蔡万概</span>
   </div>
 </template>
 
